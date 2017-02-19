3f743b849e8c4de2b7a7f0e0b40a7f687bc34003
Minh Quân <Minh Qun>
2017-02-19 21:13:11 +0700

    setup Game Window

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by QuanT on 2/19/2017.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        GameWindow gameWindow = new GameWindow();
+
+    }
+}
